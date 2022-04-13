59924c9b8b871773dad7929d0a0e901cffe02d7f
Darren Bowers <darren.bowers@g.austincc.edu>
2022-04-13 15:59:32 -0500

    initial commit

--- bintree.cpp
+++ bintree.cpp
@@ -1,7 +1,133 @@
-/*
- * Your comment header here
- */
+/*********************
+Name: Darren Bowers
+Coding 07
+Purpose: Creating a Binary Search Tree to work struct Data and  Struct Node
+**********************/
 
 #include "bintree.h"
 
+/*********************************
+CONSTRUCTORS / DESTRUCTORS
+*********************************/
 
+BinTree::BinTree(){
+
+}
+
+BinTree::~BinTree(){
+
+}
+
+
+/*********************************
+PRIVATE
+*********************************/
+
+void BinTree::clear(DataNode* root){
+
+}
+
+bool BinTree::addNode(DataNode* info, DataNode** root){
+    bool added = false;
+    return added;
+}
+
+DataNode* BinTree::removeNode(int id, DataNode* root){
+    return 0;
+}
+
+bool BinTree::getNode(Data* dataBox, int id, DataNode* root){
+    bool found = false;
+    return found;
+}
+
+bool BinTree::contains(int id, DataNode* root){
+    bool found = false;
+    return found; 
+}
+
+int BinTree::getHeight(DataNode* root){
+    return 0;
+}
+
+void BinTree::displayPreOrder(DataNode* root){
+
+}
+
+void BinTree::displayInOrder(DataNode* root){
+
+}
+
+void BinTree::displayPostOrder(DataNode* root){
+
+}
+
+DataNode* BinTree::minValueNode(DataNode* node){
+    DataNode* current = node;
+    while (current && current->left != NULL) {
+        current = current->left;
+    }
+    return current;
+}
+
+
+/*********************************
+PUBLIC
+*********************************/
+
+bool BinTree::isEmpty(){
+    bool empty = false;
+    return empty;
+}
+
+int BinTree::getCount(){
+    return count;
+}
+
+bool BinTree::getRootData(Data*){
+    bool found = false;
+    return found;
+}
+
+void BinTree::displayTree(){
+
+}
+
+void BinTree::clear(){
+
+}
+
+bool BinTree::addNode(int id, const string* info){
+    bool added = false;
+    return added;
+}
+
+bool BinTree::removeNode(int id){
+    return 0;
+}
+
+bool BinTree::getNode(Data* dataBox, int id){
+    bool found = false;
+    return found;
+}
+
+bool BinTree::contains(int id){
+    bool found = false;
+    return found; 
+}
+
+int BinTree::getHeight(){
+    return 0;
+}
+
+void BinTree::displayPreOrder(){
+
+}
+
+void BinTree::displayInOrder(){
+
+}
+
+void BinTree::displayPostOrder(){
+
+}
--- bintree.h
+++ bintree.h
@@ -1,12 +1,56 @@
-/*
- * Your comment header here
- */
+/*********************
+Name: Darren Bowers
+Coding 07
+Purpose: Creating a Binary Search Tree to work struct Data and  Struct Node
+**********************/
 
 #ifndef BINTREE_BINTREE_H
 #define BINTREE_BINTREE_H
 
+#include <iostream>
+#include "data.h"
+
+using std::cout;
+using std::endl;
+
+class BinTree {
+
+public:
+    BinTree();
+    ~BinTree();
+
+    bool isEmpty();
+    int getCount();
+    bool getRootData(Data*);
+    void displayTree();
+
+    void clear(); //has private overloads
+    bool addNode(int, const string*);
+    bool removeNode(int);
+    bool getNode(Data*, int);
+    bool contains(int);
+    int getHeight();
+    void displayPreOrder();
+    void displayInOrder();
+    void displayPostOrder();
+
+private:
+    DataNode* root;
+    int count;
 
 
+    void clear(DataNode*); //overloads
+    bool addNode(DataNode*, DataNode**);
+    DataNode* removeNode(int, DataNode*);
+    bool getNode(Data*, int, DataNode*);
+    bool contains(int, DataNode*);
+    int getHeight(DataNode*);
+    void displayPreOrder(DataNode*);
+    void displayInOrder(DataNode*);
+    void displayPostOrder(DataNode*);
+    DataNode* minValueNode(DataNode*); 
+
+};
 
 #endif /* BINTREE_BINTREE_H */
 
