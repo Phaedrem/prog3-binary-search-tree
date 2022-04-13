34d075f17a93288494f69bee77576af157eb64aa
Darren Bowers <darren.bowers@g.austincc.edu>
2022-04-13 17:13:28 -0500

    updated clear methods to delete the tree when called and set destructor to call clear. Updated isEmpty and getCount return depending on counts data. Updated getRootData and displayInOrder to print or return appropriate data.

--- bintree.cpp
+++ bintree.cpp
@@ -14,8 +14,71 @@ BinTree::BinTree(){
 
 }
 
-BinTree::~BinTree(){
+BinTree::BinTree(int ids[], string strings[]) {
+    /* this is NOT a correct constructor,
+     * you CANNOT use this assignments
+     * *****************
+     * this is an alternate constructor for demonstration
+     * purposes only. This builds a hard coded tree
+     * based on the known test data. It is simply for
+     * demonstration and proving displayInOrder traversal.
+     */
+    count = 0; 
 
+    root = new DataNode;
+    root->left = nullptr;
+    root->right = nullptr;
+    root->data.id = ids[0];
+    root->data.information = strings[0];
+    count++;
+
+    root->left = new DataNode;
+    root->left->left = nullptr;
+    root->left->right = nullptr;
+    root->left->data.id = ids[1];
+    root->left->data.information = strings[1];
+    count++;
+
+    root->right = new DataNode;
+    root->right->left = nullptr;
+    root->right->right = nullptr;
+    root->right->data.id = ids[2];
+    root->right->data.information = strings[2];
+    count++;
+
+    root->left->right = new DataNode;
+    root->left->right->left = nullptr;
+    root->left->right->right = nullptr;
+    root->left->right->data.id = ids[3];
+    root->left->right->data.information = strings[3];
+    count++;
+    
+    root->left->left = new DataNode;
+    root->left->left->left = nullptr;
+    root->left->left->right = nullptr;
+    root->left->left->data.id = ids[4];
+    root->left->left->data.information = strings[4];
+    count++;
+    
+    root->left->right->right = new DataNode;
+    root->left->right->right->left = nullptr;
+    root->left->right->right->right = nullptr;
+    root->left->right->right->data.id = ids[5];
+    root->left->right->right->data.information = strings[5];
+    count++;
+    
+    root->left->right->left = new DataNode;
+    root->left->right->left->left = nullptr;
+    root->left->right->left->right = nullptr;
+    root->left->right->left->data.id = ids[6];
+    root->left->right->left->data.information = strings[6];
+    count++;
+
+    return;
+}
+
+BinTree::~BinTree(){
+    clear(root);
 }
 
 
@@ -23,8 +86,16 @@ BinTree::~BinTree(){
 PRIVATE
 *********************************/
 
-void BinTree::clear(DataNode* root){
-
+void BinTree::clear(DataNode* localRoot){
+    if(localRoot != nullptr){
+        clear(localRoot->left);
+        clear(localRoot->right);
+        localRoot->left = nullptr;
+        localRoot->right = nullptr;
+        delete localRoot;
+        count--;
+    }
+    root = nullptr;
 }
 
 bool BinTree::addNode(DataNode* info, DataNode** root){
@@ -54,8 +125,16 @@ void BinTree::displayPreOrder(DataNode* root){
 
 }
 
-void BinTree::displayInOrder(DataNode* root){
-
+void BinTree::displayInOrder(DataNode* localRoot){
+    if(localRoot){
+        if(localRoot->left){
+            displayInOrder(localRoot->left);
+        }
+        cout << localRoot->data.id << " " << localRoot->data.information << endl;
+        if(localRoot->right){
+            displayInOrder(localRoot->right);
+        }
+    }
 }
 
 void BinTree::displayPostOrder(DataNode* root){
@@ -64,7 +143,7 @@ void BinTree::displayPostOrder(DataNode* root){
 
 DataNode* BinTree::minValueNode(DataNode* node){
     DataNode* current = node;
-    while (current && current->left != NULL) {
+    while (current && current->left != nullptr) {
         current = current->left;
     }
     return current;
@@ -76,25 +155,33 @@ PUBLIC
 *********************************/
 
 bool BinTree::isEmpty(){
-    bool empty = false;
-    return empty;
+    return count == 0; 
 }
 
 int BinTree::getCount(){
     return count;
 }
 
-bool BinTree::getRootData(Data*){
+bool BinTree::getRootData(Data* emptyData){
     bool found = false;
+    emptyData->id = -1;
+    emptyData->information = "";
+    if(root != nullptr){
+        emptyData->id = root->data.id;
+        emptyData->information = root->data.information;
+        found = true;
+    }
     return found;
 }
 
 void BinTree::displayTree(){
-
+    
 }
 
 void BinTree::clear(){
-
+    if(root != nullptr){
+        clear(root);
+    }
 }
 
 bool BinTree::addNode(int id, const string* info){
@@ -125,7 +212,12 @@ void BinTree::displayPreOrder(){
 }
 
 void BinTree::displayInOrder(){
-
+    if(root){
+        displayInOrder(root);
+    }else{
+        cout << "Tree is empty" << endl;
+    }
+    
 }
 
 void BinTree::displayPostOrder(){
